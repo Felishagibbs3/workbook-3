5ef86cf2e5faf082cd69a2d9d692906114ef8817
Felisha Gibbs <Felishagibbs3@gmail.com>
2025-04-24 10:58:19 -0500

    Format Date

---
+++ FormDates/src/main/java/com/pluralsight/formatDates.java
@@ -0,0 +1,35 @@
+package com.pluralsight;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class formatDates {
+    public static void main(String[] args) {
+
+        LocalDate now = LocalDate.now();
+        System.out.println("The date is:" + " " + now);
+        System.out.println("-----------");
+
+
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        String format1 = now.format(format);
+        System.out.println(format1);
+        System.out.println("-----------");
+
+        DateTimeFormatter format2 = DateTimeFormatter.ofPattern("MMMM, d, yyyy");
+        String formatter2 = now.format(format2);
+        System.out.println(formatter2);
+
+        System.out.println("-----------");
+
+
+        LocalDateTime dateTime = LocalDateTime.now();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("E, MMM, dd, yyyy, HH:mm");
+        String formatDate = dateTime.format(dtf);
+        System.out.println("Today is; " + formatDate);
+
+
+
+    }
+}
