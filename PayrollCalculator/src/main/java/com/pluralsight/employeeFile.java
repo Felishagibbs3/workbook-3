92f53ffcfe0d5cf3820cbf52a2e6cb637f262cb5
Felisha Gibbs <Felishagibbs3@gmail.com>
2025-04-24 08:48:09 -0500

    payroll calculator updates

--- PayrollCalculator/src/main/java/com/pluralsight/employeeFile.java
+++ PayrollCalculator/src/main/java/com/pluralsight/employeeFile.java
@@ -1,7 +1,10 @@
 package com.pluralsight;
 
 import java.io.*;
-
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.Scanner;
 
 
@@ -10,80 +13,44 @@ public class employeeFile {
 
 
         try {
+            Scanner theScanner = new Scanner(System.in);
+            System.out.println("Enter name of employee file to process");
+            String inputFile = theScanner.nextLine();
 
-            FileReader fileReader = new FileReader("src/main/resources/employees.csv");
+            System.out.println("Enter name of payroll file you'd like to create");
+            String output = theScanner.nextLine();
+
+
+            FileReader fileReader = new FileReader("src/main/resources/" + inputFile);
+            FileWriter fileWriter = new FileWriter("src/main/resources/" + output);
 
             BufferedReader bufReader = new BufferedReader(fileReader);
+            BufferedWriter write = new BufferedWriter(fileWriter);
+            write.write("id|name|gross pay\n");
+            {
 
-            String input;
-            while ((input = bufReader.readLine()) != null) {
+                String input;
+                while ((input = bufReader.readLine()) != null) {
 
-                String[] inputParts = input.split("\\|");
-                if (inputParts[0].equals("id")) {
-                    continue;
+                    String[] inputParts = input.split("\\|");
+
+                    if (inputParts[0].equals("id")) {
+                        continue;
+                    }
+
+                    Employee theEmployee = new Employee(Integer.parseInt(inputParts[0]), inputParts[1], Double.parseDouble(inputParts[2]), Double.parseDouble(inputParts[3]));
+
+                    System.out.printf("%d - %s - $%.2f\n", theEmployee.getEmployeeId(), theEmployee.getName(), theEmployee.getGrossPay());
+
                 }
 
-                Employee theEmployee = new Employee(Integer.parseInt(inputParts[0]), inputParts[1], Double.parseDouble(inputParts[2]), Double.parseDouble(inputParts[3]));
-
-                System.out.printf("%d - %s - $%.2f\n", theEmployee.getEmployeeId(), theEmployee.getName(), theEmployee.getGrossPay());
+                bufReader.close();
+                write.close();
 
             }
-
-            bufReader.close();
-
         } catch (Exception e) {
             throw new RuntimeException(e);
-
-
         }
-        Scanner theScanner = new Scanner(System.in);
-        System.out.println("Enter name of employee file to process");
-        String inputFile = theScanner.nextLine();
-
-        System.out.println("Enter name of payroll file you'd like to create");
-        String output = theScanner.nextLine();
-
-        try (
-                BufferedReader bufread = new BufferedReader(new FileReader("src/main/resources/employees.csv"));
-                BufferedWriter write = new BufferedWriter(new FileWriter("payroll-sept-2023.csv"))
-        ) {
-
-            write.write("id|name|gross pay");
-            write.newLine();
-
-            String line = bufread.readLine();
-
-            while ((line = bufread.readLine()) != null) {
-                String[] parts = line.split(",");
-                if (parts.length != 4) continue;
-
-                int iD = Integer.parseInt(parts[0]);
-                String names = parts[1];
-                double hoursWorked = Double.parseDouble(parts[2]);
-                double hourlyRate = Double.parseDouble(parts[3]);
-
-                double grossPay = hoursWorked * hourlyRate;
-
-                String Payroll = iD + "|" + names + "|" + grossPay + "\n");
-                write.write(Payroll);
-                write.newLine();
-
-                BufferedReader.close();
-                BufferedWriter.close();
-
-
-            }
-            catch(IOException e){
-                System.out.println("Error Processing Files");
-            }
-
-            System.out.println("File Successfully Created!" + output);
-
-
-        }
-
 
     }
-
-
 }
